c566a06befe37a5811bfb699d8ea81f2b82f68de
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-09-13 21:55:25 -0500

    let,const, and var

--- fundemental/partOne.js
+++ fundemental/partOne.js
@@ -1,8 +1,23 @@
 console.log("hello");
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1994
-console.log(typeof year);
+// year = 1994
+// console.log(typeof year);
+
+// -------------------------------
+
+let age = 30;
+// mutated variable
+age = 31;
+
+const birthYear = 1994
+// birthYear = 1991
+
+var job = "programmer"
+job = "teacher"
+
+lastName = 'Campo'
+console.log(lastName);
