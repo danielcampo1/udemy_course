6a53b8851823cdf0597d306024f06ca4aa672147
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-09-17 11:21:40 -0500

    true and falsy

--- fundemental/partOne.js
+++ fundemental/partOne.js
@@ -170,14 +170,36 @@ console.log("hello");
 
 
 //type conversion
-const inputYear = '1994';
-console.log(Number(inputYear));
-console.log(Number(inputYear) + 18);
+// const inputYear = '1994';
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) + 18);
 
-console.log(Number('daniel'));
+// console.log(Number('daniel'));
 
 //Coercion
 
-console.log('23' - '10' - 3); //converts to number
-console.log('23' + '10' + 3); // concats and converts to string
+// console.log('23' - '10' - 3); //converts to number
+// console.log('23' + '10' + 3); // concats and converts to string
 
+//------------------------------------------------------------
+
+//true and falsy
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('daniel'));
+console.log(Boolean({}));
+
+const money = 23;
+if(money) {
+    console.log('dont spend it all');
+} else {
+    console.log('get a job');
+}
+
+let height;
+if(height){
+    console.log('YAY!!!!!!!!!!!!');
+}else{
+    console.log('not defined homie!');
+}
