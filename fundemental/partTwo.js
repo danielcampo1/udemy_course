2542370124001c06973d975eaae1332ffa1ded3d
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-09-28 17:52:41 -0500

    strict mode

---
+++ fundemental/partTwo.js
@@ -0,0 +1,14 @@
+// Scrict Mode
+// strict mode helps create errors for us to see. and forbids us to do stuff.
+
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriverLicense = true
+if(hasDriversLicense) console.log('i can drive!!');
+
+//  unexpected strict mode reserced word with scrict mode
+// const interface = 'audio';
+// const private = 564
