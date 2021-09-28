fea57058432603c0b7b305bc4a10d2bfd135ac87
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-09-28 18:31:47 -0500

    understanding functions

--- fundemental/partTwo.js
+++ fundemental/partTwo.js
@@ -1,14 +1,42 @@
 // Scrict Mode
 // strict mode helps create errors for us to see. and forbids us to do stuff.
 
-'use strict';
+// 'use strict';
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if(passTest) hasDriverLicense = true
-if(hasDriversLicense) console.log('i can drive!!');
+// if(passTest) hasDriverLicense = true
+// if(hasDriversLicense) console.log('i can drive!!');
 
 //  unexpected strict mode reserced word with scrict mode
 // const interface = 'audio';
 // const private = 564
+
+// --------------------------------------------------------
+
+// functions
+
+// what is a function??
+    // is a variable but a chunk of code that can be recalled
+    // functions can recieve data and return data.
+    
+function logger() {
+    console.log('My name is Daniel!');
+}
+
+// calling, running, or invoking function
+logger();
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges){
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+const appleJuice = fruitProcessor(5, 0)
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);
