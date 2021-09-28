8fb43abb4019ee87dd76b794b5c0c536040ba50b
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-09-28 18:50:13 -0500

    Functions Declarations vs expressions

--- fundemental/partTwo.js
+++ fundemental/partTwo.js
@@ -1,7 +1,7 @@
 // Scrict Mode
 // strict mode helps create errors for us to see. and forbids us to do stuff.
 
-// 'use strict';
+'use strict';
 
 // let hasDriversLicense = false;
 // const passTest = true;
@@ -21,22 +21,43 @@
     // is a variable but a chunk of code that can be recalled
     // functions can recieve data and return data.
     
-function logger() {
-    console.log('My name is Daniel!');
+// function logger() {
+//     console.log('My name is Daniel!');
+// }
+
+// // calling, running, or invoking function
+// logger();
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges){
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+// const appleJuice = fruitProcessor(5, 0)
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2,4);
+// console.log(appleOrangeJuice);
+
+//------------------------------------------------------------
+
+// Functions Declarations vs expressions
+
+// Declaration
+function calcAge1(birthYear){
+    return 2040 - birthYear;
 }
 
-// calling, running, or invoking function
-logger();
-logger();
-logger();
-logger();
+const age1 = calcAge1(1994);
+console.log(age1);
 
-function fruitProcessor(apples, oranges){
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
+//expression
+
+const calcAge2 = function (birthYear) {
+    return 2040 - birthYear
 }
-const appleJuice = fruitProcessor(5, 0)
-console.log(appleJuice);
+const age2 = calcAge2(1994)
 
-const appleOrangeJuice = fruitProcessor(2,4);
-console.log(appleOrangeJuice);
+console.log(age1, age2);
