4a00fad23d68c497930292eda458be3a8c01455b
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-09-28 19:01:26 -0500

    arrow functions

--- fundemental/partTwo.js
+++ fundemental/partTwo.js
@@ -45,19 +45,36 @@
 
 // Functions Declarations vs expressions
 
-// Declaration
-function calcAge1(birthYear){
-    return 2040 - birthYear;
+// // Declaration
+// function calcAge1(birthYear){
+//     return 2040 - birthYear;
+// }
+
+// const age1 = calcAge1(1994);
+// console.log(age1);
+
+// //expression
+// const calcAge2 = function (birthYear) {
+//     return 2040 - birthYear
+// }
+// const age2 = calcAge2(1994)
+
+// console.log(age1, age2);
+
+//-------------------------------------------------------------------
+
+// Arrow Function
+
+const calcAge3 = birthYear => 2040 - birthYear;
+const age3 = calcAge3(1991)
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear;
+    const retirement = 65 - age;
+    // return retirement
+    return `${firstName} retires in ${retirement} years`;
 }
 
-const age1 = calcAge1(1994);
-console.log(age1);
-
-//expression
-
-const calcAge2 = function (birthYear) {
-    return 2040 - birthYear
-}
-const age2 = calcAge2(1994)
-
-console.log(age1, age2);
+console.log(yearsUntilRetirement(1997, 'Daniel'))
+console.log(yearsUntilRetirement(1965, 'Daniel'))
