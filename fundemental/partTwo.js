b695714e9726a623aa50ccbf37df46801e7def38
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-10-09 21:32:43 -0500

    while loop

--- fundemental/partTwo.js
+++ fundemental/partTwo.js
@@ -372,6 +372,7 @@ for(let i = 0; i < daniel.length; i++) {
 --------------------------- */
 
 //looping backwards
+/*
 
 const daniel = [
     'Daniel',
@@ -393,3 +394,25 @@ for(let exercise = 1; exercise < 4; exercise++){
         console.log(`${exercise}: Lifting weight repetition ${rep}`);
     }
  }
+ -----------------------------------*/
+
+ //while loop
+
+//  for(let rep = 1; rep <= 10; rep++){
+//     console.log(`Lifting weight repetition ${rep}`);
+// }
+
+let rep = 1
+while(rep <=10){
+    // console.log(`WHILE: Lifting weight repetition ${rep}`);
+    rep++
+};
+
+let dice = Math.trunc(Math.random() * 6) + 1
+
+while (dice !== 6){
+    console.log(`you rolled a ${dice}`);
+
+    dice = Math.trunc(Math.random() * 6) + 1
+    if(dice === 6 )console.log('Loop is about to end ...');
+}
