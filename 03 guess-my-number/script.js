6ed6a1326b6f9ecc756ab816a7d86c4abd1f469c
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-10-19 20:12:46 -0500

    game is becoming functions

--- 03 guess-my-number/script.js
+++ 03 guess-my-number/script.js
@@ -10,6 +10,11 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -17,5 +22,23 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💣 You lost the game!';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💣 You lost the game!';
+    }
   }
 });
