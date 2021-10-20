8180ed4b28fd9a6d6c10c04d725d95e767eb6c3b
Danielcampo1 <68871070+danielcampo1@users.noreply.github.com>
2021-10-19 21:02:27 -0500

    built resert button

--- 03 guess-my-number/script.js
+++ 03 guess-my-number/script.js
@@ -10,15 +10,23 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
-
 let score = 20;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+});
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
-  console.log(guess, typeof guess);
   // when no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
@@ -30,6 +38,8 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    document.querySelector('.number').textContent = secretNumber;
 
     // when guess is to high
   } else if (guess > secretNumber) {
